c527eef33b8e1ad19bdb41b3c0af04f76b5d33a2
Abner J Pelisser <abner.pelisser@gmail.com>
2022-05-20 00:02:26 -0300

    tela detalhe da receita - mod07.2

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Abner J Pelisser'
+        'name': 'Abner J Pelisser-home'
     })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={
-        'name': 'Abner J Pelisser'
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Abner J Pelisser-detail'
     })
